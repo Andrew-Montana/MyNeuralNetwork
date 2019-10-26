9d9f8d3eb103221d2bc0b7b896dca66e04cd94c6
Andrew <le09@ukr.net>
2019-10-27 01:13:09 +0200

    Add and write Layer.cs

---
+++ MyNeuralNetwork/Assets/Scripts/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer
+{
+    public List<Neuron> neurons = new List<Neuron>();
+    public int numNeurons;
+
+    public Layer(int numNeurons, int numNeuronInputs)
+    {
+        this.numNeurons = numNeurons;
+        for (int i = 0; i < numNeurons; i++)
+        {
+            neurons.Add(new Neuron(numNeuronInputs));
+        }
+    }
+}
