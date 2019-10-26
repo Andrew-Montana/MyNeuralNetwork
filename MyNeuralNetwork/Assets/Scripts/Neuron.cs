5b35705bfeae9eb1c79e602e112d96d0d4006414
Andrew <le09@ukr.net>
2019-10-27 00:42:26 +0200

    Add and write Neuron.cs

---
+++ MyNeuralNetwork/Assets/Scripts/Neuron.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neuron
+{
+    public double bias;
+    public double output;
+    public List<double> inputs = new List<double>();
+    public List<double> weights = new List<double>();
+    public int numInputs;
+    public double errorGradient;
+
+    public Neuron(int numInputs)
+    {
+        this.numInputs = numInputs;
+        bias = UnityEngine.Random.Range(-1f, 1f);
+        for (int i = 0; i < numInputs; i++)
+        {
+            weights.Add(UnityEngine.Random.Range(-1f, 1f));
+        }
+    }
+
+    public Neuron(int numInputs, List<double> weights, double bias)
+    {
+        this.numInputs = numInputs;
+        this.weights = weights;
+        this.bias = bias;
+    }
+}
